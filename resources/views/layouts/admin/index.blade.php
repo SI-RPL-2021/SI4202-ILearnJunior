35861f4beba1e29820e081384f669dd221e7a719
addityap <addityap06@gmail.com>
2021-03-21 16:20:57 +0700

    Starter Template ILearn Junior

---
+++ resources/views/layouts/admin/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts/admin/header')
+<body>
+  <div id="app">
+    <div class="main-wrapper">
+@include('layouts/admin/navbar')
+@include('layouts/admin/sidebar')
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          {{-- ISI DENGAN KONTEN --}}
+          @yield('content')
+        </section>
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2021
+        </div>
+        <div class="footer-right">
+          2.3.0
+        </div>
+      </footer>
+    </div>
+  </div>
+@include('layouts/admin/cdnjs') 
+</body>
+</html>
---
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,58 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="#">I Learn Junior</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="#">LJ</a>
+      </div>
+      <ul class="sidebar-menu">
+          <li class="menu-header">Dashboard</li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="index-0.html">Lihat Dashboard</a></li>
+              <li><a class="nav-link" href="index.html">Konfigurasi Landing Page</a></li>
+            </ul>
+          </li>
+          <li class="menu-header">Fiture</li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Management Kelas</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="components-article.html">Tambah Kelas</a></li>
+              <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Lihat Kelas</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Management Blog</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="{{route('tambah_blog')}}">Buat Postingan</a></li>
+              <li><a class="nav-link" href="forms-editor.html">Daftar Postingan</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Management Podcast</span></a>
+            <ul class="dropdown-menu">
+              <li><a href="gmaps-advanced-route.html">Tambahkan Podcast</a></li>
+              <li><a href="gmaps-draggable-marker.html">List Podcast</a></li>
+              <li><a href="gmaps-geocoding.html">Coming Soon</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Management Soon</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
+              <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
+              <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
+              <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Management User</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="modules-calendar.html">User Terdaftar</a></li>
+            </ul>
+          </li>
+        </ul>
+    </aside>
+  </div>
