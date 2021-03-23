24d452c08bdf55b4191edb4bf41f432426ec2844
dimasrzein <dimasraihanzein@gmail.com>
2021-03-23 16:19:37 +0700

    membuat tampilan management kelas ref #1

--- resources/views/admin/Kelas/add.blade.php
+++ resources/views/admin/Kelas/add.blade.php
@@ -1,8 +1,34 @@
 @extends('layouts.admin.index')
 @section('content')
 
-{{-- ngoding tambah kelas disokin --}}
 <div class="section-header">
-    <h1>Tambah Kelas</h1>
+    <h1>Management Kelas</h1>
 </div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Tambah Kelas</h4>
+                <div class="card-header-action">
+                    <a href="" class="btn btn-primary">
+                        Cari
+                    </a>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table align-items-center table-hover" id="table">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Kelas</th>
+                                <th>Tipe Kelas</th>
+                                <th>Thumbnail</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                    </div>
+                </div>
+
+
 @endsection
