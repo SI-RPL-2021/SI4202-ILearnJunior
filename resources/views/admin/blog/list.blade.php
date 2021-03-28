153907eba9e14af77c21f7ed10ef5d371e697068
addityap <addityap06@gmail.com>
2021-03-28 11:18:21 +0700

    Membuat Fiture CRUD pada management BLOG serta membuat migrasi untuk table blog ref #3

---
+++ resources/views/admin/blog/list.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin.index')
+@section('content')
+@section('menu', 'List Postingan Blog')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>List Postingan Blog</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-hover" ">
+                                <th>No</th>
+                                <th>Thumbnail Blog</th>
+                                <th>Nama Post</th>
+                                <th>Action</th>
+                                <tbody>
+                                    <?php $index = 1; ?>
+                                     @foreach ($blog as $post)
+                            <td>{{ $index++ }}</td>
+                            <td>
+                                <img src="{{asset('gambar_blog/'. $post->thumbnail_blog)}}" height="50" width="50" alt="">
+                            </td>
+                            <td>{{ $post->name_blog }}</td>
+                            <td>
+                                <a href="/detail_blog/{{ $post->id }}"class="btn btn-sm btn-success">Detail</a>
+                            </td>
+                            </tbody>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
